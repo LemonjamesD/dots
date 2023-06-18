a1c0924d661a57426431cfa92c2bf1e8cd968881
LemonjamesD <lemon@lemonjamesd.com>
2023-06-18 12:59:19 -0400

    [SCRIPT] Updated dot files! 🚀

--- users/lemon/login-manager.nix
+++ users/lemon/login-manager.nix
@@ -1,14 +1,26 @@
-{ pkgs, inputs, ... }:
+{ pkgs, user, inputs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    greetd.tuigreet
+  ];
+
   services.greetd = {
     enable = true;
-    settings = rec {
-      initial_session = {
-        command = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprland";
-        user = "lemon";
+    settings = {
+      default_session = {
+        command = ''
+          ${pkgs.greetd.tuigreet}/bin/tuigreet 
+          -g "Pure Joy, Functions." 
+          -r --remember-session 
+          --time 
+          --asterisks 
+          --sessions "/run/current-system/sw/share/wayland-sessions:/run/current-system/sw/share/xsessions"
+          --power-shutdown shutdown now
+          --power-reboot reboot
+        '';
+        user = user;
       };
-      default_session = initial_session;
     };
   };
 }

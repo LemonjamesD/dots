163fd24e8ac57f0c82472ae4caa35c4c415d6262
LemonjamesD <lemon@lemonjamesd.com>
2023-10-03 23:39:53 -0400

    [SCRIPT] Updated dot files! 🚀

--- users/shared/virtual.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "lemon" ];
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-}
-

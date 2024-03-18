168b04abaa67488d1743fbf534c628767987c659
LemonjamesD <lemon@lemonjamesd.com>
2024-03-18 15:22:27 -0400

    [SCRIPT] Updated dot files! 🚀

--- update-dots.sh
+++ update-dots.sh
@@ -17,25 +17,25 @@ git add .
 git commit -m "[SCRIPT] Updated dot files! 🚀"
 git push
 
-# Copy README to eww
-cp /etc/nixos/README.md ./
-git add README.md
-git commit -m "[SCRIPT] Updated \`eww\` README! 🚀"
-git push
+# # Copy README to eww
+# cp /etc/nixos/README.md ./
+# git add README.md
+# git commit -m "[SCRIPT] Updated \`eww\` README! 🚀"
+# git push
 
-# Update hyprland git
-cd ~/.config/hypr
-git pull
-git checkout hyprland
-git add .
-git commit -m "[SCRIPT] Updated dot files! 🚀"
-git push
+# # Update hyprland git
+# cd ~/.config/hypr
+# git pull
+# git checkout hyprland
+# git add .
+# git commit -m "[SCRIPT] Updated dot files! 🚀"
+# git push
 
-# Copy README to hyprland
-cp /etc/nixos/README.md ./
-git add README.md
-git commit -m "[SCRIPT] Updated \`hyprland\` README! 🚀"
-git push
+# # Copy README to hyprland
+# cp /etc/nixos/README.md ./
+# git add README.md
+# git commit -m "[SCRIPT] Updated \`hyprland\` README! 🚀"
+# git push
 
 # Go back
 cd $PAST
